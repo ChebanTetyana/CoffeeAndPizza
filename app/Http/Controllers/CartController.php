b92af6d77b19d7aca86e589f9f0cf3fcd1661cd0
ChebanTetyana <kapitoskay1302@gmail.com>
2024-06-07 20:52:43 +0300

    Adding products from the menu to the cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,55 +3,90 @@
 namespace App\Http\Controllers;
 
 use App\Models\MenuItem;
+use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function index() {
-        $cartItems = OrderItem::all();
-        $totalPrice = $cartItems->sum('price');
-        return view('cart.index', compact('cartItems', 'totalPrice'));
+
+        $user = Auth::user();
+        $cartItems = OrderItem::whereNull('order_id')->where('user_id', $user->id)->get();
+//        dd($cartItems);
+//        $cartItems = OrderItem::whereNull('order_id')->get();
+
+        $totalPrice = $cartItems->sum(function($item) {
+            return $item->price * $item->count;
+        });
+        $totalPricePerItems = $cartItems->keyBy('id')->map(function ($item) {
+            return $item->price * $item->count;
+        });
+
+        return view('cart.index', compact('cartItems', 'totalPrice', 'totalPricePerItems'));
+    }
+
+    public function delete($id)
+    {
+        $product = OrderItem::findOrFail($id);
+        $product->delete();
+        return redirect()->route('cart.index')->with('success', 'Item removed from cart');
     }
 
     public function addToCart(Request $request)
     {
-//        dd($request->all());
+//        dd($request);
+        $user = Auth::user();
+
+//        dd('User authenticated:', $user);
         $validatedData = $request->validate([
             'product_id' => 'required|exists:menu_items,id',
-            'size' => 'required',
+            'size' => 'nullable',
             'price' => 'required|numeric',
         ]);
 
-        $productId = $validatedData['product_id'];
-        $size = $validatedData['size'];
-        $price = $validatedData['price'];
 
+        $productId = $validatedData['product_id'];
+//        dd($validatedData);
+        $size = $validatedData['size'];
+//        dd($validatedData);
+
+        $price = $validatedData['price'];
+//        dd($validatedData);
         $product = MenuItem::find($productId);
         if (!$product) {
-            return response()->json(['message' => 'Продукт не найден'], 404);
+//            dd($validatedData);
+            return response()->json(['message' => 'Product not found'], 404);
         }
-
+//        dd($product);
         $order_id = null;
         $cartItem = new OrderItem();
         $cartItem->name = $product->name;
-        $cartItem->size = $size;
-        $cartItem->price = $price;
+        $cartItem->size = $product->product_type === 'promotion' ? null : $size;
+        $cartItem->price = $product->product_type === 'promotion' ? 6.00 : $price;
         $cartItem->image = $product->image;
         $cartItem->product_type = $product->product_type;
         $cartItem->order_id = $order_id;
         $cartItem->count = 1;
+        $cartItem->user_id = $user->id;
+
 //        dd($cartItem);
         $cartItem->save();
-
+//        dd($cartItem);
         return redirect()->route('cart.index');
     }
 
     public function getCartItemCount()
     {
-        $count = OrderItem::count();
+        $user = Auth::user();
+        if (!$user) {
+            return response()->json(['message' => 'User not authenticated'], 401);
+        }
+
+        $count = OrderItem::whereNull('order_id')->where('user_id', $user->id)->count();
         return response()->json(['count' => $count]);
     }
-
 
 }
---
+++ public/js/counter.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+        let decreaseButtons = document.querySelectorAll('.decrease-btn');
+        let increaseButtons = document.querySelectorAll('.increase-btn');
+
+        decreaseButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            let id = this.getAttribute('data-id');
+            let countElement = this.nextElementSibling;
+            let count = parseInt(countElement.textContent);
+            if (count > 1) {
+                count--;
+                countElement.textContent = count;
+                updateTotalPrice(id, count);
+                updateTotalOrderPrice();
+            }
+        });
+    });
+
+    increaseButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            let id = this.getAttribute('data-id');
+            let countElement = this.previousElementSibling;
+            let count = parseInt(countElement.textContent);
+            count++;
+            countElement.textContent = count;
+            updateTotalPrice(id, count);
+            updateTotalOrderPrice();
+        });
+    });
+
+    function updateTotalPrice(id, count) {
+        let pricePerItemElement = document.querySelector(`[data-id="${id}"].price-per-item`);
+        let totalPriceElement = document.querySelector(`[data-id="${id}"].total-price`);
+
+        if (pricePerItemElement && totalPriceElement) {
+            let pricePerItem = parseFloat(pricePerItemElement.textContent.replace(/[^\d.]/g, ''));
+            totalPriceElement.textContent = (pricePerItem * count).toFixed(2);
+        }
+
+        // fetch(`/cart/update/${id}?count=${count}`, { method: 'POST' });
+    }
+
+    function updateTotalOrderPrice() {
+        let totalPriceElements = document.querySelectorAll('.total-price');
+        let totalPrice = Array.from(totalPriceElements).reduce((sum, element) => {
+            return sum + parseFloat(element.textContent.replace(/[^\d.]/g, ''));
+        }, 0);
+
+        document.getElementById('totalPrice').textContent = 'Total price: ' + totalPrice.toFixed(2);
+    }
+
+
+
+});
+
