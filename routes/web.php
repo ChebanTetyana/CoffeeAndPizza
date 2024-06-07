3449e600a9407e3fa2b31e9423333fc7abcc8199
ChebanTetyana <kapitoskay1302@gmail.com>
2024-06-07 20:57:41 +0300

    Create routs and separate in authorization groups

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
@@ -17,22 +18,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::group(['middleware'=>'auth'], function (){
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+    Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+    Route::get('menu/pizza', [MenuController::class, 'getPizzas'])->name('pizza.index');
+    Route::get('menu/coffee', [MenuController::class, 'getCoffee'])->name('coffee.index');
+    Route::get('/get-price', [MenuController::class, 'getPrice']);
+
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
+    Route::get('/cart/count', [CartController::class, 'getCartItemCount']);
+    Route::delete('cart/{id}', [CartController::class, 'delete'])->name('cart.delete');
+
+    Route::post('/order/create', [OrderController::class, 'create'])->name('order.create');
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::group(['middleware'=>'guest'], function (){
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+//    Route::get('/about', [HomeController::class, 'about'])->name('about.index');
+});
 Route::get('/about', [HomeController::class, 'about'])->name('about.index');
 
-Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
-Route::get('menu/pizza', [MenuController::class, 'getPizzas'])->name('pizza.index');
-Route::get('menu/coffee', [MenuController::class, 'getCoffee'])->name('coffee.index');
-Route::get('/get-price', [MenuController::class, 'getPrice']);
 
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
-Route::get('/cart/count', [CartController::class, 'getCartItemCount']);
 
-Route::get('/orders/create', [OrderController::class, 'create'])->name('order.create');
+
 
